c54b8deedd8d4b79cae66a85905b6438ef09af0e
andersonmcook <andersonmcook@gmail.com>
2016-03-26 13:29:52 -0500

    using refs

--- App.js
+++ App.js
@@ -5,18 +5,34 @@ class App extends React.Component {
   constructor () {
     super()
     this.state = {
-      txt: 'this is the state text'
+      red: 0,
+      green: 0,
+      blue: 0
     }
     this.update = this.update.bind(this)
   }
 
   update (event) {
-    this.setState({txt: event.target.value})
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+    })
   }
 
   render () {
     return (
-      <Widget txt={this.state.txt} update={this.update} />
+      <div>
+        <Slider ref='red' update={this.update} />
+        {this.state.red}
+        <br />
+        <Slider ref='green' update={this.update} />
+        {this.state.green}
+        <br />
+        <Slider ref='blue' update={this.update} />
+        {this.state.blue}
+        <br />
+      </div>
     )
   }
 // before widget implemented
@@ -37,6 +53,16 @@ App.propTypes = {
 
 App.defaultProps = {
   txt: 'default text'
+}
+
+class Slider extends React.Component {
+  render () {
+    return (
+      <div>
+        <input ref='inp' type='range' min='0' max='255' onChange={this.props.update} />
+      </div>
+    )
+  }
 }
 
 const Widget = (props) => {
